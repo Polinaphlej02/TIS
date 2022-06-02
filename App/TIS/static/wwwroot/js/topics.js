40f52f1fb77a5f000bf6107b3034ef2c04d5704f
Alexander Astashenok <a.astashenok@syberry.com>
2022-06-02 14:36:24 +0300

    suka

--- App/TIS/static/wwwroot/js/topics.js
+++ App/TIS/static/wwwroot/js/topics.js
@@ -2,6 +2,6 @@ document.getElementById('log-out-btn').addEventListener('click', () => {
     document.querySelector('.confirm-log-out__wrapper').style.display = 'block';
 })
 
-document.getElementById('cancel-log-out').addEventListener('click', () => {
+document.querySelector('.cancel-log-out').addEventListener('click', () => {
     document.querySelector('.confirm-log-out__wrapper').style.display = 'none';
 })
